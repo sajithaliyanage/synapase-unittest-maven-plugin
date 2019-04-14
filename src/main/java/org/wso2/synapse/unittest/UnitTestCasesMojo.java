848e5b12f4296f27dc0effcff4d93838613da682
sajithaliyanage <sajithaliyanage@gmail.com>
2019-04-14 12:14:46 +0530

    update testing mvn plugin

--- src/main/java/org/wso2/synapse/unittest/UnitTestCasesMojo.java
+++ src/main/java/org/wso2/synapse/unittest/UnitTestCasesMojo.java
@@ -150,10 +150,19 @@ public class UnitTestCasesMojo extends AbstractMojo {
             BufferedReader stdInput = new BufferedReader(new
                     InputStreamReader(proc.getInputStream()));
 
-            // read the output from the command
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(proc.getErrorStream()));
+
+// read the output from the command
             System.out.println("Here is the standard output of the command:\n");
-            String s;
+            String s = null;
             while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+// read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
                 System.out.println(s);
             }
 
